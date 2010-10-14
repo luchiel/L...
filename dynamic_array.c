6154c7f71246b3d596499fe3cd1c925eaa56cac8
luchiel <luchiella@yandex.ru>
2010-10-14 23:14:31 +1100

    Dynamic arrays: combing

--- dynamic_array.c
+++ dynamic_array.c
@@ -1,7 +1,6 @@
 #include <string.h>
+#include <assert.h>
 #include "linear_sequence.h"
-
-#define IteratorInvalid NULL
 
 typedef
 	enum
@@ -38,9 +37,11 @@ static void reallocIfFreeSpace(LSQ_HandleT handle);
 static LSQ_IteratorT createIterator(LSQ_HandleT handle)
 {
 	ArrayIteratorPointerT iterator;
-	if(handle == LSQ_HandleInvalid) return LSQ_HandleInvalid;
+	if(handle == NULL) return LSQ_HandleInvalid;
+
 	iterator = (ArrayIteratorPointerT)malloc(sizeof(ArrayIteratorT));
-	if(iterator == IteratorInvalid) return IteratorInvalid;
+	if(iterator == NULL) return LSQ_HandleInvalid;
+
 	iterator->container = handle;
 	return iterator;
 }
@@ -72,62 +73,70 @@ static void reallocIfFreeSpace(LSQ_HandleT handle)
 	}
 }
 
-extern LSQ_HandleT LSQ_CreateSequence(void)
+LSQ_HandleT LSQ_CreateSequence(void)
 {
 	ArrayPointerT handle = malloc(sizeof(ArrayT));
-	if(handle == LSQ_HandleInvalid) return LSQ_HandleInvalid;
+
+	if(handle == NULL) return LSQ_HandleInvalid;
+	
 	handle->elements = NULL;
 	handle->count = 0;
 	handle->size = 0;
 	return handle; 
 }
 
-extern void LSQ_DestroySequence(LSQ_HandleT handle)
+void LSQ_DestroySequence(LSQ_HandleT handle)
 {
-	if(handle != LSQ_HandleInvalid)
+	if(handle != NULL)
 	{
 		free(((ArrayPointerT)handle)->elements);
 		free(handle);
 	}
 }
 
-extern LSQ_IntegerIndexT LSQ_GetSize(LSQ_HandleT handle)
+LSQ_IntegerIndexT LSQ_GetSize(LSQ_HandleT handle)
+{		
+	return handle == NULL ? -1 : ((ArrayPointerT)handle)->count;
+}
+
+int LSQ_IsIteratorDereferencable(LSQ_IteratorT iterator)
 {	
-	if(handle == LSQ_HandleInvalid)
-		return -1;
-	else
-		return ((ArrayPointerT)handle)->count;
+	return
+		iterator == NULL ? 0 :
+		((ArrayIteratorPointerT)iterator)->type == IT_DEREFERENCABLE;
 }
 
-extern int LSQ_IsIteratorDereferencable(LSQ_IteratorT iterator)
+int LSQ_IsIteratorPastRear(LSQ_IteratorT iterator)
 {
-	if(iterator == IteratorInvalid) return -1;
-	return ((ArrayIteratorPointerT)iterator)->type == IT_DEREFERENCABLE;
+	
+	return
+		iterator == NULL ? 0 :
+		((ArrayIteratorPointerT)iterator)->type == IT_PASTREAR;
 }
 
-extern int LSQ_IsIteratorPastRear(LSQ_IteratorT iterator)
-{
-	if(iterator == IteratorInvalid) return -1;
-	return ((ArrayIteratorPointerT)iterator)->type == IT_PASTREAR;
+int LSQ_IsIteratorBeforeFirst(LSQ_IteratorT iterator)
+{	
+	return
+		iterator == NULL ? 0 :
+		((ArrayIteratorPointerT)iterator)->type == IT_BEFOREFIRST;
 }
 
-extern int LSQ_IsIteratorBeforeFirst(LSQ_IteratorT iterator)
-{
-	if(iterator == IteratorInvalid) return -1;
-	return ((ArrayIteratorPointerT)iterator)->type == IT_BEFOREFIRST;
-}
-
-extern LSQ_BaseTypeT * LSQ_DereferenceIterator(LSQ_IteratorT iterator)
+LSQ_BaseTypeT * LSQ_DereferenceIterator(LSQ_IteratorT iterator)
 {
 	ArrayIteratorPointerT it = (ArrayIteratorPointerT)iterator;
-	if(it == IteratorInvalid) return IteratorInvalid;
+	
+	if(it == NULL) return LSQ_HandleInvalid;
+	assert(it->container != NULL);
+
 	return it->container->elements + it->index;
 }
 
-extern LSQ_IteratorT LSQ_GetElementByIndex(LSQ_HandleT handle, LSQ_IntegerIndexT index)
+LSQ_IteratorT LSQ_GetElementByIndex(LSQ_HandleT handle, LSQ_IntegerIndexT index)
 {
 	ArrayIteratorPointerT iterator = createIterator(handle);
-	if(iterator == IteratorInvalid) return IteratorInvalid;
+
+	if(iterator == NULL) return LSQ_HandleInvalid;
+
 	iterator->index = index;
 	if(index < 0)
 	{
@@ -150,37 +159,40 @@ extern LSQ_IteratorT LSQ_GetElementByIndex(LSQ_HandleT handle, LSQ_IntegerIndexT
 	return iterator;
 }
 
-extern LSQ_IteratorT LSQ_GetFrontElement(LSQ_HandleT handle)
+LSQ_IteratorT LSQ_GetFrontElement(LSQ_HandleT handle)
 {
 	return LSQ_GetElementByIndex(handle, 0);
 }
 
-extern LSQ_IteratorT LSQ_GetPastRearElement(LSQ_HandleT handle)
+LSQ_IteratorT LSQ_GetPastRearElement(LSQ_HandleT handle)
 {
-	if(handle == LSQ_HandleInvalid) return LSQ_HandleInvalid;
-	return LSQ_GetElementByIndex(handle, ((ArrayPointerT)handle)->count);	
+	return handle == NULL ?
+		LSQ_HandleInvalid :
+		LSQ_GetElementByIndex(handle, ((ArrayPointerT)handle)->count);	
 }
 
-extern void LSQ_DestroyIterator(LSQ_IteratorT iterator)
+void LSQ_DestroyIterator(LSQ_IteratorT iterator)
 {
-	if(iterator != IteratorInvalid)
+	if(iterator != NULL)
 		free(iterator);
 }
 
-extern void LSQ_AdvanceOneElement(LSQ_IteratorT iterator)
+void LSQ_AdvanceOneElement(LSQ_IteratorT iterator)
 {
 	LSQ_ShiftPosition(iterator, 1);
 }
 
-extern void LSQ_RewindOneElement(LSQ_IteratorT iterator)
+void LSQ_RewindOneElement(LSQ_IteratorT iterator)
 {
 	LSQ_ShiftPosition(iterator, -1);
 }
 
-extern void LSQ_ShiftPosition(LSQ_IteratorT iterator, LSQ_IntegerIndexT shift)
+void LSQ_ShiftPosition(LSQ_IteratorT iterator, LSQ_IntegerIndexT shift)
 {
 	ArrayIteratorPointerT it = (ArrayIteratorPointerT)iterator;
-	if(it == IteratorInvalid) return;
+
+	if(it == NULL) return;
+	
 	it->index += shift;
 	if(it->index < 0)
 	{
@@ -201,60 +213,73 @@ extern void LSQ_ShiftPosition(LSQ_IteratorT iterator, LSQ_IntegerIndexT shift)
 	}
 }
 
-extern void LSQ_SetPosition(LSQ_IteratorT iterator, LSQ_IntegerIndexT pos)
+void LSQ_SetPosition(LSQ_IteratorT iterator, LSQ_IntegerIndexT pos)
 {
-	if(iterator == IteratorInvalid) return;
+	if(iterator == NULL) return;
 	((ArrayIteratorPointerT)iterator)->index = 0;
 	LSQ_ShiftPosition(iterator, pos);
 }
 
-extern void LSQ_InsertFrontElement(LSQ_HandleT handle, LSQ_BaseTypeT element)
+void LSQ_InsertFrontElement(LSQ_HandleT handle, LSQ_BaseTypeT element)
 {
 	ArrayIteratorPointerT it = LSQ_GetFrontElement(handle);
-	LSQ_InsertElementBeforeGiven(it, element);	
+	LSQ_InsertElementBeforeGiven(it, element);
+	LSQ_DestroyIterator(it);
 }
 
-extern void LSQ_InsertRearElement(LSQ_HandleT handle, LSQ_BaseTypeT element)
+void LSQ_InsertRearElement(LSQ_HandleT handle, LSQ_BaseTypeT element)
 {
 	ArrayIteratorPointerT it = LSQ_GetPastRearElement(handle);
 	LSQ_InsertElementBeforeGiven(it, element);	
+	LSQ_DestroyIterator(it);
 }
 
-extern void LSQ_InsertElementBeforeGiven(LSQ_IteratorT iterator, LSQ_BaseTypeT newElement)
+void LSQ_InsertElementBeforeGiven(LSQ_IteratorT iterator, LSQ_BaseTypeT newElement)
 {
 	LSQ_BaseTypeT * placeOfElement = NULL;
 	ArrayIteratorPointerT it = ((ArrayIteratorPointerT)iterator);
-	if(it == IteratorInvalid || it->container == LSQ_HandleInvalid) return;
+	
+	if(it == NULL) return;
+	assert(it->container != NULL);
+
 	reallocIfOverflow(it->container);	
 	placeOfElement = it->container->elements + it->index;
-	memmove(placeOfElement + 1, placeOfElement, sizeof(LSQ_BaseTypeT) * (it->container->count - it->index));	
+	memmove(
+		placeOfElement + 1, placeOfElement,
+		sizeof(LSQ_BaseTypeT) * (it->container->count - it->index));	
 	* placeOfElement = newElement;
 	it->container->count++;
 }
 
-extern void LSQ_DeleteFrontElement(LSQ_HandleT handle)
+void LSQ_DeleteFrontElement(LSQ_HandleT handle)
 {
 	ArrayIteratorPointerT it = LSQ_GetFrontElement(handle);
 	LSQ_DeleteGivenElement(it);
+	LSQ_DestroyIterator(it);
 }
 
-extern void LSQ_DeleteRearElement(LSQ_HandleT handle)
+void LSQ_DeleteRearElement(LSQ_HandleT handle)
 {
 	ArrayIteratorPointerT it = LSQ_GetPastRearElement(handle);
 	LSQ_RewindOneElement(it);
 	LSQ_DeleteGivenElement(it);	
+	LSQ_DestroyIterator(it);
 }
 
-extern void LSQ_DeleteGivenElement(LSQ_IteratorT iterator)
+void LSQ_DeleteGivenElement(LSQ_IteratorT iterator)
 {		
 	LSQ_BaseTypeT * placeOfElement;
 	ArrayIteratorPointerT it = ((ArrayIteratorPointerT)iterator);
-	if(it == IteratorInvalid || it->type != IT_DEREFERENCABLE) return;
-	if(it->container == LSQ_HandleInvalid) return;
-	placeOfElement = it->container->elements + it->index;	
+
+	if(!LSQ_IsIteratorDereferencable(it)) return;
+	assert(it->container != NULL);
+	
 	if (it->container->count > 0)
 	{
-		memmove(placeOfElement, placeOfElement + 1, sizeof(LSQ_BaseTypeT) * (it->container->count - it->index - 1));
+		placeOfElement = it->container->elements + it->index;		
+		memmove(
+			placeOfElement, placeOfElement + 1,
+			sizeof(LSQ_BaseTypeT) * (it->container->count - it->index - 1));
 		it->container->count--;
 		reallocIfFreeSpace(it->container);
     }	
